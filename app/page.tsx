206c3816367d4f99bb5ab1a0b16d844dc721691b
mofareed-97 <m.fareeed1997@gmail.com>
2023-07-28 01:08:56 +0300

    hero

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,5 @@
-import AboutSection from "@/components/about-section";
-import GlobeVisitors from "@/components/globe-visitors";
 import HeroSection from "@/components/hero-section";
 import UploadImage from "@/components/upload-image";
-import { getSession } from "next-auth/react";
 
 export default async function Home() {
   return (
@@ -12,8 +9,7 @@ export default async function Home() {
     from-70%
     to-pink-500/10"
     >
-      {/* <HeroSection /> */}
-      <AboutSection />
+      <HeroSection />
       <UploadImage />
       <footer className="py-16 flex flex-col items-center text-muted-foreground">
         <p className="">Made With Love</p>
--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -1,96 +1,53 @@
-"use client";
-import React, { useEffect } from "react";
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { ArrowUpRight, Sparkle } from "lucide-react";
+import React from "react";
+import GlobeVisitors from "./globe-visitors";
 import Image from "next/image";
-import Balancer from "react-wrap-balancer";
-import { Icons } from "./icons";
-import { stagger, useAnimate, usePresence } from "framer-motion";
+import { Balancer } from "react-wrap-balancer";
+import { Button } from "./ui/button";
+
 function HeroSection() {
-  const [scope, animate] = useAnimate();
-  const [isPresent, safeToRemove] = usePresence();
-
-  useEffect(() => {
-    if (isPresent) {
-      const enterAnimation = async () => {
-        await animate(
-          "span",
-          { opacity: 1, y: [50, 700] },
-          {
-            duration: 5,
-            ease: "linear",
-            delay: stagger(2),
-            repeat: Infinity,
-            repeatDelay: 0,
-          }
-        );
-      };
-      enterAnimation();
-    } else {
-      const exitAnimation = async () => {
-        await animate("span", { opacity: 0, y: 0 });
-        safeToRemove();
-      };
-
-      exitAnimation();
-    }
-  }, [isPresent]);
-
   return (
-    <section className="flex  min-h-screen flex-col justify-center items-center relative">
-      <div className="pointer-events-none ">
-        <div
-          ref={scope}
-          // className="mask-grid absolute inset-0 rounded-2xl  opacity-100"
-          className="absolute inset-0 rounded-2xl  opacity-100"
-          style={{
-            maskImage: "radial-gradient(700px 400px, white, transparent)",
-            WebkitMaskImage: "radial-gradient(700px 400px, white, transparent)",
-          }}
-        >
-          <span className="absolute z-30 h-[1rem] w-[1rem] rounded-[9999px] blur-sm bg-orange-700 shadow-[0_0_0_1px_#ffffff10] rotate-[180deg] top-0 left-1/4 before:content-[''] before:h-20 before:w-[1px] before:-top-20 before:left-[7px] before:bg-gradient-to-t before:from-orange-400 before:to-sky-400 before:absolute"></span>
-          <span className="absolute z-30 h-[1rem] w-[1rem] rounded-[9999px] blur-sm bg-orange-700 shadow-[0_0_0_1px_#ffffff10] rotate-[180deg] top-0 left-1/2 before:content-[''] before:h-20 before:w-[1px] before:-top-20 before:left-[7px] before:bg-gradient-to-t before:from-orange-400 before:to-sky-400  before:absolute"></span>
-          <span className="absolute z-30 h-[1rem] w-[1rem] rounded-[9999px] blur-sm bg-orange-700 shadow-[0_0_0_1px_#ffffff10] rotate-[180deg] top-0 right-1/4 before:content-[''] before:h-20 before:w-[1px] before:-top-20 before:left-[7px] before:bg-gradient-to-t before:from-orange-400 before:to-sky-400 before:absolute"></span>
-          <Icons.grid />
-        </div>
-      </div>
-      <div className="bg-foreground/10 backdrop-blur-lg opacity-90 text-xs py-2 px-4 rounded-full flex items-center gap-1 hover:-translate-y-1 duration-700 cursor-pointer">
-        <p className="">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet eaque
-        </p>
-        <Sparkle className="w-4 h-4" />
-      </div>
-      <div className="text-center z-10">
-        <h1 className="text-7xl my-14">
-          <Balancer>
-            Pixel Perfection Made
-            <br />
-            Easy With rePix
-          </Balancer>
-        </h1>
-        <p className="text-muted-foreground">
-          Enhance, Refine, and Shine Your Visuals on
-          <br />
-          Our Image Enhancing Platform!
-        </p>
-        <div className="flex max-w-lg mx-auto bg-foreground/10 backdrop-blur-lg opacity-90 rounded-full overflow-hidden z-10 items-center my-10">
-          <Input
-            className="bg-transparent py-6 px-5 border-none outline-none"
-            placeholder="Your email.."
-          />
-          <Button
-            variant={"ghost"}
-            className="whitespace-nowrap border border-white/10 mr-5 py-5 rounded-full"
-          >
-            Book a demo
-          </Button>
-        </div>
-        <div className="flex items-center justify-center gap-4 py-10">
-          <Button className="w-32">{"Let's"} Try</Button>
-          <Button className="w-32" variant={"outline"}>
-            Contact Us
-          </Button>
+    // <section className="border overflow-hidden h-screen mx-4 rounded-2xl relative bg-background">
+    <section className="h-screen relative ">
+      <div className="max-w-7xl px-6 2xl:px-0 mx-auto h-full flex flex-col justify-center">
+        <div className="flex pt-32 sm:pt-24 lg:pt-0 flex-col px-6  text-center lg:text-left lg:px-0 lg:flex-row lg:items-start gap-10 ">
+          <div className="">
+            <h1 className="animate-fade-up bg-gradient-to-br from-foreground to-stone-500 bg-clip-text text-4xl font-bold tracking-[-0.02em] text-transparent  drop-shadow-sm sm:text-5xl xl:text-7xl md:leading-[5rem]">
+              <Balancer>Upscale Your Images with Our Enhancement</Balancer>
+            </h1>
+            <p className="lg:max-w-xl text-muted-foreground my-10">
+              Are your photos not looking as stunning as you want them to be?
+              {"Don't"} worry; {"we've"} got you covered! Our Image Quality
+              Improvement website is here to transform your ordinary images into
+              extraordinary masterpieces
+            </p>
+            <div className="flex items-center justify-center lg:justify-start gap-5">
+              <Button>Enhance Now</Button>
+              <Button variant={"outline"}>Contact Me</Button>
+            </div>
+          </div>
+          <div className="flex justify-center gap-6 lg:gap-0 mt-6 lg:mt-0 lg:flex-row-reverse  items-center xl:pb-32">
+            <div className="w-44 md:w-52 lg:w-96 rounded-lg h-44 md:h-52 lg:h-96 overflow-hidden bg-gradient-to-t  from-background to-muted border border-gray-800 shadow-xl relative -translate-x-0  lg:-translate-x-20  ">
+              <Image
+                src={"/men-before.jpg"}
+                alt="Photo by https://unsplash.com/@elishavision"
+                fill
+                sizes="25vw"
+                priority
+                className="object-cover blur-[1px]"
+              />
+            </div>
+            <div className="w-44 md:w-52 lg:w-96 rounded-lg overflow-hidden h-44 md:h-52 lg:h-96 bg-gradient-to-t z-10 from-background to-muted border border-gray-800 shadow-xl -translate-x-0 lg:translate-y-60 lg:translate-x-40">
+              <Image
+                src={"/men-before.jpg"}
+                alt="before"
+                fill
+                sizes="25vw"
+                className="object-cover"
+                priority
+              />
+            </div>
+          </div>
+          {/* <GlobeVisitors /> */}
         </div>
       </div>
     </section>
@@ -98,3 +55,18 @@ function HeroSection() {
 }
 
 export default HeroSection;
+
+{
+  /* <div
+aria-hidden="true"
+className="z-10 bg-[linear-gradient(90deg,_#000_0%,_#ffffff00_0%,_#8f8f8f_50%,_#000_100%)] left-1/2 bottom-0 w-[300px] max-w-[300px] user-select-none center pointer-events-none absolute h-px  -translate-x-1/2 -translate-y-1/2"
+/>
+<div
+aria-hidden="true"
+className="-bottom-1 left-1/2 h-[200px] w-full max-w-[200px] md:max-w-[400px] user-select-none center pointer-events-none absolute  -translate-x-1/2 -translate-y-1/2"
+style={{
+  background:
+    "conic-gradient(from 90deg at 50% 50%, #00000000 50%, #000 50%),radial-gradient(rgba(200, 200, 200, 0.1) 0%, transparent 80%)",
+}}
+/> */
+}
